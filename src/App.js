2f1ec3e9b0ab92b0c952b13ce31488eab27d690a
ivandi1980 <ivandi.djoh@gmail.com>
2019-08-17 16:03:23 +0800

    added pages home and about, added BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react'
-import Users from './components/Users';
+import { BrowserRouter, Route } from 'react-router-dom'
 
-/* ini adalah CLASS COMPONENT
-   untuk Parent component seperti ini kita membuatnya menggunakan Class Component
-   karena kita akan menyimpan State didalamnya
-   dan untuk mengakses Statenya nanti akan kita alihkan ke Child Component menggunakan PROPS
-*/
+import Home from './components/Home'
+import About from './components/About'
+import Users from './components/Users'
 
 export class App extends Component {
 
-    state = {
-        users : [
-            { id: 1, nama: 'Arjuna Djoh', umur: 4, hobby: 'Nonton Yutube' },
-            { id: 2, nama: 'Dave Djoh', umur: 10, hobby: 'Bermain Game' },
-            { id: 3, nama: 'John Doe', umur: 12, hobby: 'Bermain Gitar' },
-            { id: 4, nama: 'Jane Doe', umur: 10, hobby: 'Bernyanyi' }
-        ]
-    }
-
     render() {
         return (
-            <div className="container">
-                <Users kumpulanUsers={ this.state.users } />
-            </div>
+            <BrowserRouter>
+                <div className="container">
+                    <Route path='/' component={Home} />
+                    <Route path='/about' component={About} />
+                    <Route path='/user' component={Users} />
+                </div>
+            </BrowserRouter>
         )
     }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Users = () => {
+const Home = () => {
     return (
         <div>
-            <h1>USERS</h1>
+            <h1>HOME</h1>
             <p>
                 Et ut dolore cillum sunt. Aute non occaecat enim anim excepteur eiusmod quis incididunt irure ad enim. Exercitation amet consectetur occaecat veniam proident consequat cupidatat fugiat culpa ut reprehenderit aliquip sit do.
             </p>
@@ -11,4 +11,4 @@ const Users = () => {
     )
 }
 
-export default Users;
+export default Home;
