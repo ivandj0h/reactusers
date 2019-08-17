5418f5aa73575d9e213e0f2b0805503a00f68e85
ivandi1980 <ivandi.djoh@gmail.com>
2019-08-17 17:00:20 +0800

    added navbar and navlink

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Users from './components/Users'
+import Navbar from './components/Navbar';
 
 export class App extends Component {
 
     render() {
         return (
             <BrowserRouter>
+                <Navbar />
                 <div className="container">
-                    <Route path='/' component={Home} />
+                    <Route exact path='/' component={Home} />
                     <Route path='/about' component={About} />
                     <Route path='/user' component={Users} />
                 </div>
---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <Link className="navbar-brand" to="/">USERAPP</Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarColor02">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <Link to="/" className="nav-link">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/about" className="nav-link">About</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/user" className="nav-link">User</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
