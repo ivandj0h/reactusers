bfa862f3366d1f8e07032a1d6e2681895d310e9d
ivandi1980 <ivandi.djoh@gmail.com>
2019-08-17 15:29:11 +0800

    added more style for ui component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 import Users from './components/Users';
 
+/* ini adalah CLASS COMPONENT
+   untuk Parent component seperti ini kita membuatnya menggunakan Class Component
+   karena kita akan menyimpan State didalamnya
+   dan untuk mengakses Statenya nanti akan kita alihkan ke Child Component menggunakan PROPS
+*/
+
 export class App extends Component {
 
     state = {
         users : [
-            { id: 1, nama: 'Arjuna Tirta Ramadhan Djoh', umur: 4, hobby: 'bermain game' }
+            { id: 1, nama: 'Arjuna Djoh', umur: 4, hobby: 'Nonton Yutube' },
+            { id: 2, nama: 'Dave Djoh', umur: 10, hobby: 'Bermain Game' },
+            { id: 3, nama: 'John Doe', umur: 12, hobby: 'Bermain Gitar' },
+            { id: 4, nama: 'Jane Doe', umur: 10, hobby: 'Bernyanyi' }
         ]
     }
 
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,15 +1,23 @@
 import React from 'react'
 
-// untuk child component seperti ini kita membuat UI Component
-// tanpa menggunakan Class tapi menggunakan Functional Component
+/* ini adalah UI COMPONENT
+   untuk child component seperti ini kita membuat UI Component
+   tanpa menggunakan Class tapi menggunakan Functional Component
+*/
+
 const Users = ({kumpulanUsers}) => {
 
+    // Selanjutnya kita akan coba menggunakan kondisional
+    
         const listUsers = kumpulanUsers.map(users => {
+            let listStyle = users.umur >= 11 ? ('card text-white bg-success mb-3') : ( null )
+
             return (
-                <div className="card bg-light mb-3">
-                    <div className="card-body" key={users.id}>
+                <div className={listStyle || "card text-white bg-danger mb-3"} key={users.id}>
+                    <div className="card-body">
                         <h4 className="card-title"><b>{users.nama}</b></h4>
-                        <p className="card-text"><b>{users.hobby}</b></p>
+                        <p className="card-text">Umur : <b>{users.umur}</b> Tahun</p>
+                        <p className="card-text">Hobby : <b>{users.hobby}</b></p>
                     </div>
                 </div>
             )
